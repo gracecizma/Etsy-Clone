07d5c30c9a675177a41ee51d5f3ec7b375073056
Brennan Cota <brennancota@yahoo.com>
2023-03-14 13:09:10 -0700

    added logout button styling

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -45,10 +45,12 @@ function ProfileButton({ user }) {
       <ul className={ulClassName} ref={ulRef}>
         {user ? (
           <>
-            <li>{user.username}</li>
+            <li>Hello {user.username}</li>
             <li>{user.email}</li>
             <li>
-              <button onClick={handleLogout}>Log Out</button>
+              <button className="logout-button" onClick={handleLogout}>
+                Log Out
+              </button>
             </li>
           </>
         ) : (
