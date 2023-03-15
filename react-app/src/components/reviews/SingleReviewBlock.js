4b1f3c587d3500780714b0dfe15d1807f06fd514
Sarah <kretaksson@gmail.com>
2023-03-15 08:42:27 -0700

    simple review component

---
+++ react-app/src/components/reviews/SingleReviewBlock.js
@@ -0,0 +1,19 @@
+import { useSelector } from "react-redux"
+
+function SingleReviewBlock({review}){
+ user = useSelector(state=>state.session.user)
+ product = useSelector()
+    
+    return (
+         <div className="SingleReviewBlock"> 
+            <div className="ReviewBlock">
+            <p className="username_on_Review">{user.username}'s review</p>
+                <div className="RatingsBlock">
+                <p>{user.username}'s Rating for </p>
+
+                </div>
+            </div>
+         </div>)
+}
+
+export default SingleReviewBlock
