b5cb8b13f0d300c766f02f40e5141355fb76d1ff
gracecizma <gracecizma@gmail.com>
2023-03-15 11:23:19 -0500

    order table created

---
+++ app/models/order.py
@@ -0,0 +1,32 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class order(db.Model):
+    __tablename__ = "orders"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod("products.id")), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod("users.id")), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    order_id = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+    user = db.relationship("User", back_populates="orders")
+    products = db.relationship("Product", back_populates="orders")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "product_id": self.product_id,
+            "user_id": self.user_id,
+            "quantity": self.quantity,
+            "order_id": self.order_id,
+            "total_price": self.total_price,
+            "date": self.date
+        }
--- app/models/shopping_cart.py
+++ app/models/shopping_cart.py
@@ -10,7 +10,9 @@ class ShoppingCart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.ForeignKey(add_prefix_for_prod(
         "products.id")), nullable=False)
-    user_id = db.Column(db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    user_id = db.Column(db.ForeignKey(
+        add_prefix_for_prod('users.id')), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
 
     product = db.relationship("Product", back_populates="shopping_carts")
     user = db.relationship("User", back_populates="shopping_carts")
@@ -20,5 +22,6 @@ class ShoppingCart(db.Model):
             "id": self.id,
             "product_id": self.product_id,
             "products": [product.to_dict() for product in self.products],
-            "user_id": self.user_id
+            "user_id": self.user_id,
+            "quantity": self.quantity
         }
