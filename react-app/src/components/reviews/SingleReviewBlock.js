f33b648c29d83d701cc2858af18752303d4b4029
Sarah <kretaksson@gmail.com>
2023-03-16 15:36:49 -0700

    main block paginated sort of

--- react-app/src/components/reviews/SingleReviewBlock.js
+++ react-app/src/components/reviews/SingleReviewBlock.js
@@ -1,31 +1,24 @@
 import { useSelector, useDispatch } from "react-redux"
-import { getSingleProduct } from "../../store/products"
+// import { getSingleProduct } from "../../store/products"
 
-let review = { id: 1, user_id: 2, product_id: 3, comment: "this is the comment", stars: 5 }
-function SingleReviewBlock(review = "No review Supplied", product = "No Product Supplied", shop = "No Shop Supplied") {
-    const user = useSelector((state) => { return state?.session?.User })
-    //  product = useSelector(state=>state.product."currentProduct or something
-    //  similar"
-
-    if (product = "No Product Supplied" ){
-       product = {name:"No product Chosen"}
-    }
-
-product = {}
+// let review = { id: 1, user_id: 2, product_id: 3, comment: "this is the comment", stars: 5 }
+function SingleReviewBlock({ review }) {
+    const user = useSelector((state) => state.session.User)
+    const product = useSelector(state => state.product.SingleProduct)
 
 
-        return (
-            <div className="SingleReviewBlock">
-                        <h7 className="RatingStars"></h7>
-                <div className="ReviewBlock">
-                    <p className="username_on_Review">{user?.username}'s review for {product.name}</p>
+
+    return (
+        <div className="SingleReviewBlock">
+            <div className="ReviewBlock">
+                <p className="username_on_Review">{user?.username}'s review for {product.name}</p>
+                <div className="RatingsBlock">
+                    <p>*StarsIcon*:{review.stars}</p>
                     <p className="ReviewContent"> {review.comment}</p>
-                    <div className="RatingsBlock">
-                        <p>{user?.username}'s Rating for {product.name} </p>
 
-                    </div>
                 </div>
-            </div>)
+            </div>
+        </div>)
 }
 
 export default SingleReviewBlock
